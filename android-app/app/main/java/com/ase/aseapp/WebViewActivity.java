3528300f09b81e84145250d72801591d1d392c36
Arunkumar45678 <sikha.arun@gmail.com>
2025-12-29 15:25:14 +0530

    Implement authentication in WebView requests

--- android-app/app/main/java/com/ase/aseapp/WebViewActivity.java
+++ android-app/app/main/java/com/ase/aseapp/WebViewActivity.java
@@ -1,22 +1,62 @@
 package com.ase.aseapp;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
+        webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
 
+        webView.setWebViewClient(new AuthWebViewClient());
         setContentView(webView);
 
         webView.loadUrl("https://testase.gt.tc");
+    }
+
+    private class AuthWebViewClient extends WebViewClient {
+
+        @Override
+        public WebResourceResponse shouldInterceptRequest(
+                WebView view, WebResourceRequest request) {
+
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            if (user == null) return super.shouldInterceptRequest(view, request);
+
+            try {
+                String token = user.getIdToken(false).getResult().getToken();
+
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+
+                return super.shouldInterceptRequest(
+                        view,
+                        new WrappedRequest(request, headers)
+                );
+
+            } catch (Exception e) {
+                return super.shouldInterceptRequest(view, request);
+            }
+        }
     }
 }
