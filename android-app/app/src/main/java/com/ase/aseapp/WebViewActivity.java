e74c2c712ad44849419ab3eaac1bdade15ba2b97
Arunkumar45678 <sikha.arun@gmail.com>
2025-12-31 10:23:09 +0530

    Update WebViewActivity.java

--- android-app/app/src/main/java/com/ase/aseapp/WebViewActivity.java
+++ android-app/app/src/main/java/com/ase/aseapp/WebViewActivity.java
@@ -1,62 +1,81 @@
 package com.ase.aseapp;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+    private ProgressBar spinner;
+    private SwipeRefreshLayout swipeRefresh;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
 
-        webView = new WebView(this);
+        webView = findViewById(R.id.webView);
+        spinner = findViewById(R.id.loadingSpinner);
+        swipeRefresh = findViewById(R.id.swipeRefresh);
+
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
 
         webView.setWebViewClient(new AuthWebViewClient());
-        setContentView(webView);
 
+        loadPage();
+
+        swipeRefresh.setOnRefreshListener(() -> {
+            loadPage();
+            swipeRefresh.setRefreshing(false);
+        });
+    }
+
+    private void loadPage() {
+        if (!isOnline()) {
+            webView.loadUrl("file:///android_asset/offline.html");
+            return;
+        }
         webView.loadUrl("https://testase.gt.tc");
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnected();
     }
 
     private class AuthWebViewClient extends WebViewClient {
 
         @Override
-        public WebResourceResponse shouldInterceptRequest(
-                WebView view, WebResourceRequest request) {
+        public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+            spinner.setVisibility(View.VISIBLE);
+        }
 
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            spinner.setVisibility(View.GONE);
+        }
 
-            if (user == null) return super.shouldInterceptRequest(view, request);
-
-            try {
-                String token = user.getIdToken(false).getResult().getToken();
-
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Authorization", "Bearer " + token);
-
-                return super.shouldInterceptRequest(
-                        view,
-                        new WrappedRequest(request, headers)
-                );
-
-            } catch (Exception e) {
-                return super.shouldInterceptRequest(view, request);
-            }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Authorization", "Bearer " + TokenStore.getIdToken());
+            view.loadUrl(request.getUrl().toString(), headers);
+            return true;
         }
     }
 }
